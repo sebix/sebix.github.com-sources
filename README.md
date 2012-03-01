b197335e7109db5d7baa39fc5fb45c65f3188b75
sebix <szebi@gmx.at>
2012-03-01 17:03:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# sebix.github.com sources
+
+In this repo you can find the sources of my homepage
+[sebix.github.com](http://sebix.github.com/).
+
+The output created by (acrylamid)[https://github.com/sebix/acrylamid] can be found
+(elsewhere)[https://github.com/sebix/sebix.github.com].
+
+So this repo is only for demonstration purposes.
+
+* *content/* source files for all texts
+* *layouts/* files for the layout and other static files which have to be in output/
+* *conf.py* Configuration
---
+++ conf.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+# This is your config file.  Please write in a valid python syntax!
+# See http://acrylamid.readthedocs.org/en/latest/conf.py.html
+
+SITENAME = "sebix"
+WWW_ROOT = "http://sebix.github.com/"
+
+AUTHOR = "sebix"
+EMAIL = "info@example.org" # not used anywhere
+
+# hyphenate is needed for nice justification
+FILTERS = ["markdown+codehilite(css_class=highlight)", "hyphenate"]
+VIEWS = {
+    "/": {"filters": ["summarize", "h1"],
+          "pagination": "/page/:num",
+          "view": "index"},
+    "/:year/:slug/": {"filters": ["h1"], "view": "entry"},
+    "/atom/": {"filters": ["h2"], "view": "atom"},
+    "/rss/": {"filters": ["h2"], "view": "rss"},
+    "/articles/": {"view": "articles"},
+    #"/atom/full": {"filters": ["h2"], "view": "atom", "num_entries": 1000},
+    "/tag/:name/": {"filters": ["h1", "summarize"], "view":"tag",
+                   "pagination": "/tag/:name/:num"},
+    }
+
+PERMALINK_FORMAT = "/:year/:slug/index.html"
+DATE_FORMAT = "%d.%m.%Y, %H:%M"
